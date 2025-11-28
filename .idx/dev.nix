c3ab254eb2bb93ac0ff318a15d2a10f34e9b75c7
wahyulamani03 <wahyulamani03@gmail.com>
2025-11-28 06:50:11 +0000

    add new package ssl for prisma using v5.22

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,6 +5,7 @@
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
+    pkgs.openssl.dev
     pkgs.nodejs_21
     pkgs.yarn
     pkgs.nodePackages.pnpm
